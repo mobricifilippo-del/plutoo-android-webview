b65787ebf3eb7942191547e76c3642358d9699d7
mobricifilippo-del <mobrici.filippo@live.com>
2025-10-08 11:53:08 +0200

    Create MainActivity.java

---
+++ app/app/src/main/app/src/main/java/com/plutoo/androidwebview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.plutoo.androidwebview;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://bricifilippo-del.github.io/"); // URL della tua app Plutoo
+    }
+}
