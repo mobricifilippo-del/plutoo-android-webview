9556eafb30651072f184f97c980049b861bb8d91
mobricifilippo-del <mobrici.filippo@live.com>
2026-06-12 20:14:30 +0200

    Update MainActivity.java

--- app/src/main/java/com/plutoo/app/MainActivity.java
+++ app/src/main/java/com/plutoo/app/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        MobileAds.initialize(this, initializationStatus -> {});
+
         // Layout contenitore
         FrameLayout layout = new FrameLayout(this);
 
