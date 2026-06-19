e275deb4681db8975b32a2ccce4d6e98ab226658
mobricifilippo-del <mobrici.filippo@live.com>
2026-06-19 17:19:46 +0200

    Update MainActivity.java

--- app/src/main/java/com/plutoo/app/MainActivity.java
+++ app/src/main/java/com/plutoo/app/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.graphics.Bitmap;
 import android.webkit.JavascriptInterface;
-import android.webkit.MimeTypeMap;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
