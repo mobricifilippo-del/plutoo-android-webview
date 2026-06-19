970a0bafb7ddd7b8e33df56986d33367ee705855
mobricifilippo-del <mobrici.filippo@live.com>
2026-06-19 15:33:05 +0200

    Update MainActivity.java

--- app/src/main/java/com/plutoo/app/MainActivity.java
+++ app/src/main/java/com/plutoo/app/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.Gravity;
 import android.view.View;
 import android.graphics.Bitmap;
